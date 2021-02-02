9585137df722d008054e90008cbba5f86acfa3bd
Jution Candra Kirana <jution.kirana@enigmacamp.com>
2021-02-02 11:29:18 +0700

    [IMPROVE] contoh activity input name

--- app/src/main/java/com/enigmacamp/pertemuanpertama/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/pertemuanpertama/MainActivity.kt
@@ -3,27 +3,24 @@ package com.enigmacamp.pertemuanpertama
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tvCounter: MaterialTextView
-    private var startCounter = 0
+    private lateinit var textInputName: TextInputEditText
+    private lateinit var tvName: MaterialTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        tvCounter = findViewById(R.id.tv_counter)
+        textInputName = findViewById(R.id.text_input_name)
+        tvName = findViewById(R.id.tv_name)
     }
 
-    fun notifyIncrease(view: View) {
-        startCounter+=1
-        tvCounter.text = startCounter.toString()
+    fun showName(view: View) {
+        val name = textInputName.text //Jution Candra Kirana
+        tvName.text = name
     }
-    fun notifyDecrease(view: View) {
-        startCounter-=1
-        tvCounter.text = startCounter.toString()
-    }
-
 }
