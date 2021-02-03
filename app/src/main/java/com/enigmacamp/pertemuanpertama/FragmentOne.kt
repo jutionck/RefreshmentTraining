3f21655d3cd2550f5831d68a71fd063b3a70fab8
Jution Candra Kirana <jution.kirana@enigmacamp.com>
2021-02-03 16:55:58 +0700

    [IMPROVE] - Fragment dengan view model

--- app/src/main/java/com/enigmacamp/pertemuanpertama/FragmentOne.kt
+++ app/src/main/java/com/enigmacamp/pertemuanpertama/FragmentOne.kt
@@ -1,16 +1,18 @@
 package com.enigmacamp.pertemuanpertama
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModel
 import kotlinx.android.synthetic.main.fragment_one.*
 
 class FragmentOne : Fragment() {
 
-    private var showCounter = 0
+    private val counterViewModel: CounterViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,18 +27,12 @@ class FragmentOne : Fragment() {
     //Setelah semua berjalan, logic coding taruh di sini
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val activity = activity as MainActivity
-
         increaseButton.setOnClickListener {
-            showCounter++
-            activity.controllerData(showCounter)
+          counterViewModel.increase()
+            Log.d("FRAGMENT_ONE", "Increment")
         }
         decreaseButton.setOnClickListener {
-            showCounter--
-            activity.controllerData(showCounter)
+           counterViewModel.decrease()
         }
     }
-
-
 }
